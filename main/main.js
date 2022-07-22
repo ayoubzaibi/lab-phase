5e4cb20b436d4a7dd6ea44d0eb1ce885030ccb19
zaibi ayoub <ademzaibi@gmail.com>
2022-07-22 15:23:17 +0100

    some adding to shop page

--- main/main.js
+++ main/main.js
@@ -1,80 +1,76 @@
-var plus = document.querySelectorAll('.plus');
-console.log(plus);
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
-for (let i = 0; i < plus.length; i++) {
-    plus[i].addEventListener('click', inc);
+
+if (bar) {
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active');
+    })
 }
 
-function inc (e) {
-    var btn = e.target;
-    console.log(btn);
-    var div = btn.parentElement;
-    console.log(div);
-    var count = Number(div.querySelector('p').innerHTML);
-    console.log(count);
-    count++;
-    div.querySelector('p').innerHTML = count;
-    var tr=div.parentElement.parentElement
-    console.log(tr)
-    var up=Number(tr.querySelector('.unitPrice').innerHTML);
-    var price=count*up
-    tr.querySelector('.price').innerHTML=price
-
-    somme()
-
+if (close) {
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+    })
 }
 
-var minus = document.querySelectorAll('.minus');
-console.log(minus);
+var btn=document.getElementById("my-btn")
 
-for (let i = 0; i < minus.length; i++) {
-   minus[i].addEventListener('click', deinc);
-}
-
-function deinc (e) {
-    var btn = e.target;
-    console.log(btn);
-    var div = btn.parentElement;
-    console.log(div);
-    var count = Number(div.querySelector('p').innerHTML);
-    if (count>0) {
-        console.log(count);
-    count--;
-    div.querySelector('p').innerHTML = count;
-
-    var tr=div.parentElement.parentElement
-    console.log(tr)
-    var up=Number(tr.querySelector('.unitPrice').innerHTML);
-    var price=count*up
-    tr.querySelector('.price').innerHTML=price
-    somme()
-    }
+btn.addEventListener('click',clicked)
+function clicked(e) {
+    alert("product added to the cart"
+    )
     
-
 }
 
-var prices=document.querySelectorAll('.price')
-function somme(){
-    var tot=0
-    for (let i = 0; i < prices.length; i++) {
-       tot+=Number (prices[i].innerHTML)
-       document.getElementById('total').innerHTML=tot;
 
-        
-    }
 
-}
 
-var del=document.querySelectorAll('.delete')
-for (let i = 0; i < del.length; i++) {
- del[i].addEventListener('click',dell);
 
-}
-function dell(e){
-    var cible=e.target
-    var tr=cible.parentElement
-    console.log(tr);
-    tr.querySelector('.price').innerHTML=0
-    somme();
-    tr.remove()
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
